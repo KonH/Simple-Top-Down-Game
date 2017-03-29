6409910919af15253b0a9c59130b7b696433a8b0
KonH <konh@yandex.ru>
2017-03-29 19:57:29 +0700

    Project hierarchy setup; Layers; Dummy sprite; Sample scene & prefabs; First movable;

---
+++ Assets/Scripts/Interface/PlayerMovable.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Movable))]
+public class PlayerMovable : MonoBehaviour {
+
+	public string AxisX = "Horizontal";
+	public string AxisY = "Vertical";
+
+	Movable _movable = null;
+
+	void Start() {
+		_movable = GetComponent<Movable>();
+	}
+
+	void Update() {
+		_movable.MoveVector.x = Input.GetAxis(AxisX);
+		_movable.MoveVector.y = Input.GetAxis(AxisY);
+	}
+}
---
+++ Assets/Scripts/Logics/Movable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Movable : MonoBehaviour {
+
+	public Vector2 MoveVector = Vector2.zero;
+	public float   MoveScale  = 1.0f;
+
+	Rigidbody2D _rigidbody = null;
+
+	void Start() {
+		_rigidbody = GetComponent<Rigidbody2D>();
+	}
+
+	void FixedUpdate() {
+		_rigidbody.velocity = MoveVector * MoveScale;
+	}
+}
